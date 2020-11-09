c20ce5b9f6540801010dc51d86aa4b2993ce0955
joshua <javanmega0@gmail.com>
2020-11-09 19:57:54 +0300

    links to social site updtaed

--- src/constants/socialLinks.js
+++ src/constants/socialLinks.js
@@ -11,12 +11,12 @@ const data = [
   {
     id: 1,
     icon: <FaFacebookSquare className="social-icon"></FaFacebookSquare>,
-    url: "https://www.twitter.com",
+    url: "https://www.facebook.com/javan.hash.3",
   },
   {
     id: 2,
     icon: <FaLinkedin className="social-icon"></FaLinkedin>,
-    url: "https://www.twitter.com",
+    url: "https://www.linkedin.com/in/agega-joshua-63b212182/",
   },
   {
     id: 3,
@@ -31,7 +31,7 @@ const data = [
   {
     id: 5,
     icon: <FaTwitterSquare className="social-icon"></FaTwitterSquare>,
-    url: "https://www.twitter.com",
+    url: "https://twitter.com/400_nigzz",
   },
 ]
 const links = data.map(link => {
